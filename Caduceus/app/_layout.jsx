96c18fced45cc12c7bcd433ce50486dc1de97d80
Arthur B. <a.berret.hnlt@gmail.com>
2024-09-26 02:01:26 +0200

    Repo Cleanup
    deleting empty folder + moving team info to readme.md

--- Caduceus/app/_layout.jsx
+++ Caduceus/app/_layout.jsx
@@ -6,9 +6,6 @@ const RootLayout = () => {
 
     return (
         <>
-        
-        
-
         <Stack>
             <Stack.Screen name = "index" options = {{headerShown : false}}  />
             <Stack.Screen name = "appointments" options = {{headerShown : true}}  />
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # PPE-2024
+
+Team number : #PPE24-P-376
+
+Team members :
+
+- Arthur BERRET
+- Paul ROUXEL
+- Jules DIAS
+- Constance DEAL
+- Maëlys NAINVILLE
+- Gabrielle LECLERC 
